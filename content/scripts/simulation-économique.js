ab07cd59ba5045efba0a25aa2ca3801b2c627886
Gus <guillaume1.dubus@gmail.com>
2025-02-08 11:30:52 +0100

    Improve simulation script

--- content/scripts/simulation-économique.js
+++ content/scripts/simulation-économique.js
@@ -2,11 +2,16 @@
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+	Body = Matter.Body,
+	Events = Matter.Events,
+    Composite = Matter.Composite,
+	Composites = Matter.Composites,
+	Common = Matter.Common,
+    Bodies = Matter.Bodies;
 
 // create an engine
-var engine = Engine.create();
+var engine = Engine.create(),
+	world = engine.world;
 
 // create a renderer
 var render = Render.create({
@@ -14,13 +19,53 @@ var render = Render.create({
     engine: engine
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var bodyStyle = { fillStyle: '#222' };
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+// scene code
+Composite.add(world, [
+	Bodies.rectangle(400, 0, 800, 50, { isStatic: true, render: bodyStyle }),
+	Bodies.rectangle(400, 600, 800, 50, { isStatic: true, render: bodyStyle }),
+	Bodies.rectangle(800, 300, 50, 600, { isStatic: true, render: bodyStyle }),
+	Bodies.rectangle(0, 300, 50, 600, { isStatic: true, render: bodyStyle })
+]);
+
+var stack = Composites.stack(70, 100, 9, 4, 50, 50, function(x, y) {
+	return Bodies.circle(x, y, 15, { restitution: 1, render: bodyStyle });
+});
+
+Composite.add(world, stack);
+engine.gravity.scale = 0;
+
+var shakeScene = function(engine) {
+	var bodies = Composite.allBodies(engine.world);
+
+	for (var i = 0; i < bodies.length; i++) {
+		var body = bodies[i];
+
+		var forceMagnitude = 0.03 * body.mass;
+
+		// apply the force over a single update
+		Body.applyForce(body, body.position, { 
+			x: (forceMagnitude + Common.random() * forceMagnitude) * Common.choose([1, -1]), 
+			y: -forceMagnitude + Common.random() * -forceMagnitude
+		});
+	}
+};
+
+var a = true;
+// an example of using beforeUpdate event on an engine
+Events.on(engine, 'beforeUpdate', function(event) {
+	var engine = event.source;
+	if (a) {
+		a = false;
+		shakeScene(engine);
+	}
+	var bodies = Composite.allBodies(engine.world);
+
+	for (var i = 0; i < bodies.length; i++) {
+		Body.setSpeed(bodies[i], 10);
+	}
+});
 
 // run the renderer
 Render.run(render);
