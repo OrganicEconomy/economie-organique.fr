3a54dd492d3ded5dd6b3e567fc6b531d6e4a515c
Gus <guillaume1.dubus@gmail.com>
2025-02-08 16:06:45 +0100

    Simulations : first case ok

--- content/scripts/simulation-économique.js
+++ content/scripts/simulation-économique.js
@@ -6,6 +6,7 @@ var Engine = Matter.Engine,
 	Events = Matter.Events,
     Composite = Matter.Composite,
 	Composites = Matter.Composites,
+	MouseConstraint = Matter.MouseConstraint,
 	Common = Matter.Common,
     Bodies = Matter.Bodies;
 
@@ -16,10 +17,14 @@ var engine = Engine.create(),
 // create a renderer
 var render = Render.create({
     element: document.getElementById("tatatoto"),
-    engine: engine
+    engine: engine,
+	options: {
+		wireframes: false,
+		background: "#BCAA99"
+	}
 });
 
-var bodyStyle = { fillStyle: '#222' };
+var bodyStyle = { fillStyle: '#4d8b31' };
 
 // scene code
 Composite.add(world, [
@@ -29,12 +34,15 @@ Composite.add(world, [
 	Bodies.rectangle(0, 300, 50, 600, { isStatic: true, render: bodyStyle })
 ]);
 
+bodyStyle = { fillStyle: '#4d8b31' };
+
 var stack = Composites.stack(70, 100, 9, 4, 50, 50, function(x, y) {
 	return Bodies.circle(x, y, 15, { restitution: 1, render: bodyStyle });
 });
 
-Composite.add(world, stack);
-engine.gravity.scale = 0;
+const mouseConstraint = MouseConstraint.create(
+  engine, {element: document.getElementById("tatatoto")}
+);
 
 var shakeScene = function(engine) {
 	var bodies = Composite.allBodies(engine.world);
@@ -43,6 +51,7 @@ var shakeScene = function(engine) {
 		var body = bodies[i];
 
 		var forceMagnitude = 0.03 * body.mass;
+
 
 		// apply the force over a single update
 		Body.applyForce(body, body.position, { 
@@ -55,17 +64,62 @@ var shakeScene = function(engine) {
 var a = true;
 // an example of using beforeUpdate event on an engine
 Events.on(engine, 'beforeUpdate', function(event) {
-	var engine = event.source;
-	if (a) {
-		a = false;
-		shakeScene(engine);
-	}
 	var bodies = Composite.allBodies(engine.world);
 
 	for (var i = 0; i < bodies.length; i++) {
-		Body.setSpeed(bodies[i], 10);
+		Body.setSpeed(bodies[i], 5);
 	}
 });
+
+var b = true;
+// an example of using collisionStart event on an engine
+Events.on(engine, 'collisionStart', function(event) {
+	var pairs = event.pairs;
+
+	// change object colours to show those starting a collision
+	for (var i = 0; i < pairs.length; i++) {
+		var pair = pairs[i];
+
+		if (! pair.bodyA.isStatic && ! pair.bodyB.isStatic) {
+			var scaleA = 2 / ( 2 * Math.PI * pair.bodyA.circleRadius);
+			var scaleB = 2 / ( 2 * Math.PI * pair.bodyB.circleRadius);
+			if (b) {
+				if (pair.bodyB.circleRadius > 3) {
+					Matter.Body.scale(pair.bodyA, 1 + scaleA, 1 + scaleA);
+					Matter.Body.scale(pair.bodyB, 1 - scaleB, 1 - scaleB);
+				}
+				b = false;
+			} else {
+				if (pair.bodyA.circleRadius > 3) {
+					Matter.Body.scale(pair.bodyB, 1 + scaleB, 1 + scaleB);
+					Matter.Body.scale(pair.bodyA, 1 - scaleA, 1 - scaleA);
+				}
+				b = true;
+			}
+		}
+	}
+
+});
+
+var pause = false;
+var firstClick = false;
+Events.on(mouseConstraint, "mousedown", () => {
+	if (! firstClick) {
+		shakeScene(engine);
+		firstClick = true;
+		return;
+	}
+	if (pause) {
+		engine.timing.timeScale = 1;
+		pause = false;
+	} else {
+		engine.timing.timeScale = 0;
+		pause = true;
+	}
+});
+
+Composite.add(world, stack);
+engine.gravity.scale = 0;
 
 // run the renderer
 Render.run(render);
